b2d1538109ccf7e8cf9b70ccb998835fe42ee140
Marcus Lee <marcustutorial@hotmail.com>
2021-12-06 22:48:49 +0800

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
 
-pip install pipenv 
+# install git
+sudo yum update && sudo yum install git -y
 
-pipenv install python-dotenv --dev
-pipenv install flask flask_expects_json pymysql boto3 argon2-cffi faker
-pipenv shell
+# install dev tools for build
+sudo yum groupinstall "Development Tools" -y
+sudo yum install openssl-devel -y
 
-python app.py
+# install pyenv
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# install python 3.9.9
+yes Y | pyenv install 3.9.9
+
+# install pipenv
+pip3 install pipenv
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bashrc
+source ~/.bashrc
+nvm install node
+
+# install pnpm
+npm install -g pnpm
+
+# clone repo
+git clone https://github.com/marcustut/aws-live.git
+
+# cd into repo
+cd aws-live
+
+# install virtual env (pipenv)
+yes Y | pipenv shell
+
+# install dependencies
+pipenv install && pipenv install --dev
+
+# copy .env from bucket
+aws s3 cp s3://marcuslee-aws-live-env/.env .env
+
+# install web dependencies
+cd web && pnpm i && pnpm build
+
+# run prod server
+cd .. && pipenv run python app.py
+
